b634beb85cdfe9ee06a935a2195b6b718f61644c
Xuhao <hao.xu@quadas.com>
2017-07-18 12:07:08 +0800

    Extend AroundLogger middleware

--- lib/utils/misc/http_middleware/around_logger.rb
+++ lib/utils/misc/http_middleware/around_logger.rb
@@ -2,7 +2,7 @@
 #
 # ==== Example
 #   conn = Faraday.new(url: 'http://quadas.com') do |f|
-#     f.use :around_logger do |logger|
+#     f.use :around_logger, logger: Logger.new(..), write_body: { threshold: 100 } do |logger|
 #       logger.tagged 'SSP Audit', 'Nex'
 #       # Logs "[SSP Audit] [Nex] Started POST: http://..."
 #     end
@@ -12,10 +12,12 @@ module Utils
   module Misc
     module HTTPMiddleware
       class AroundLogger < Faraday::Middleware
+        WRITE_RESPONSE_BODY_THRESHOLD = 200
 
-        def initialize(app, logger = Rails.logger)
+        def initialize(app, options = {})
           super(app)
-          @logger = logger
+          @options = options
+          @logger = options[:logger].presence || Rails.logger
           yield self if block_given?
         end
 
@@ -26,6 +28,7 @@ module Utils
           @logger.info "#{tags_text}Request body: #{request_env[:body]}"
           @app.call(request_env).on_complete do |response_env|
             ended_at   = Time.now
+            @logger.info "#{tags_text}Response body: #{response_env[:body]}" if write_body?(response_env[:response_headers]['Content-Length'])
             @logger.info "#{tags_text}Completed #{response_env[:status]} in #{((ended_at - started_at)* 1000).to_i}ms"
           end
         end
@@ -37,9 +40,18 @@ module Utils
         private
 
         def tags_text
-          if @tags.any?
-            @tags.collect { |tag| "[#{tag}] " }.join
+          @tags_text ||= begin
+            if @tags.any?
+              @tags.collect { |tag| "[#{tag}] " }.join
+            end
           end
+        end
+
+        def write_body?(body_length)
+          return false unless @options.key?(:write_body)
+          return true if @options[:write_body] === true
+          threshold = (@options[:write_body]&.[](:threshold).presence || WRITE_RESPONSE_BODY_THRESHOLD)
+          body_length.to_i <= threshold.to_i
         end
       end
     end
